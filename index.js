b57fdceddf45d6d5d86407e43b5704e27351e4ab
Unknown <paoolkim@gmail.com>
2019-11-01 15:54:28 -0700

    get and insert

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cashid = new CashID('auth.cashid.org', '/');
 
 const bodyParser = require('body-parser');
 
+const conf = require('./config/config');
+const env = process.env.NODE_ENV || 'development';
+const knex = require('knex')(conf[`${env}`]);
+
 const server = express();
 const port = 4000;
 server.use(bodyParser.json());
@@ -22,6 +26,29 @@ function parseRequest(uri) {
   return cashid.parseCashIDRequest(uri);
 }
 
+async function getRequests(action, data) {
+  let rows = await knex('Requests').where({
+    action,
+    data
+  });
+  console.log('rows', rows);
+
+  return rows;
+}
+
+async function saveRequest(cashIDObj, parsed) {
+  const { request, address, signature } = cashIDObj;
+
+  let rows = await knex('Requests').insert({
+    request,
+    address,
+    signature,
+    raw: parsed
+  });
+
+  return rows;
+}
+
 server.post('/api/auth', async (req, res) => {
   const { request, address, signature } = req.body;
   const cashIDObj = { request, address, signature };
@@ -32,12 +59,10 @@ server.post('/api/auth', async (req, res) => {
     return res.status(500).send(error);
   }
 
-  const test = parseRequest(request);
-  console.log('test', test);
-  // create db entry with timestamp
-  // combine with req body
+  const parsed = parseRequest(request);
 
-  return res.status(200).send(test);
+  await saveRequest(cashIDObj, parsed);
+  return res.status(200).send({ status: 'successs' });
 });
 
 server.post('/api/request', async (req, res) => {
@@ -79,6 +104,13 @@ server.post('/validate', (req, res) => {
 
   let valid = validateRequest(cashIDObj);
   res.send(valid);
+});
+
+server.get('/test', async (req, res) => {
+  const data = await getRequests('test', 'test');
+  console.log('data', data);
+
+  return res.status(200).send(data);
 });
 
 server.get('/', async (req, res) => {
